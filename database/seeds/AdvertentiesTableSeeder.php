f0790ecbe2b6c36d18107465bb036d01f30b77af
KeesKezer <Kees_keijser@hotmail.com>
2018-12-19 14:47:07 +0100

    Added seeder for Advertenties Table

--- database/seeds/AdvertentiesTableSeeder.php
+++ database/seeds/AdvertentiesTableSeeder.php
@@ -11,6 +11,27 @@ class AdvertentiesTableSeeder extends Seeder
      */
     public function run()
     {
-        
+         DB::table('Advertenties')->insert([
+             [
+                'titel' => 'Honda 500',
+                'omschrijving' => 'Te koop: Honda 500 uit 1987. In goede staat, met 100.000 km op de teller',
+                'ini_prijs' => '1200'
+             ]
+             [
+                'titel' => 'Honda 400',
+                'omschrijving' => 'Te koop: Honda 400 uit 1986. In goede staat, met 100.000 km op de teller',
+                'ini_prijs' => '1100'
+             ]
+             [
+                'titel' => 'Honda 300',
+                'omschrijving' => 'Te koop: Honda 300 uit 1985. In goede staat, met 100.000 km op de teller',
+                'ini_prijs' => '1200'
+             ]
+             [
+                'titel' => 'Honda 200',
+                'omschrijving' => 'Te koop: Honda 200 uit 1984. In goede staat, met 100.000 km op de teller',
+                'ini_prijs' => '1000'
+             ]
+        ]);
     }
 }
