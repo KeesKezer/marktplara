a53ea6cbcd9653710ed769114498016563373e8a
KeesKezer <Kees_keijser@hotmail.com>
2018-12-21 15:07:21 +0100

    added changes to style in css

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.layout')
 @section('content')
 @foreach($advertentie as $key => $value)
-{{$value-> id}}
-{{$value-> omschrijving}}
-{{$value-> ini_prijs}}
+ <div class="card text-center">
+  <img class="card-img-top" src="" alt="Card image cap">
+  <div class="card-body">
+      <h5 class="card-title"><a href="{{$value-> tid}}" class="card-link">{{$value-> titel}}</a></h5>
+
+    <p class="card-text"><small class="text-muted">{{$value-> ini_prijs}}<br>{{$value-> omschrijving}}</small></p>
+  </div>
+</div>
+
+
+
 @endforeach
 
 @stop
